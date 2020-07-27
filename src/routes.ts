e14ed97ce83d169836fe346306d03e6b9b3489c8
vitorueno <vitoruenootto@gmail.com>
2020-07-26 23:29:10 -0300

    add database connection

--- src/routes.ts
+++ src/routes.ts
@@ -2,4 +2,8 @@ import express from "express";
 
 const routes = express.Router();
 
+routes.get('/', (request, response) => {
+    return response.json({ message: 'Hello world' })
+});
+
 export default routes;
