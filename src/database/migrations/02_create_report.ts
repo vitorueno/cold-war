b06128de9c174e523026514d705bbf641f305849
vitorueno <vitoruenootto@gmail.com>
2020-07-26 23:56:47 -0300

    add report migration

--- src/database/migrations/02_create_report.ts
+++ src/database/migrations/02_create_report.ts
@@ -2,19 +2,15 @@
 import Knex from 'knex';
 
 export async function up(knex: Knex) {
-    return knex.schema.createTable('mission', table => {
+    return knex.schema.createTable('report', table => {
         table.increments('id').primary();
         table.string('title').notNullable();
         table.string('description').notNullable();
-        table.string('text_button_1').notNullable();
-        table.string('text_button_2').notNullable();
-        table.integer('href_button_1').notNullable();
-        table.integer('href_button_2').notNullable();
         table.integer('id_chapter').notNullable();
         table.foreign('id_chapter').references('id').inTable('chapter');
     });
 }
 
 export async function down(knex: Knex){
-    return knex.schema.dropTable('mission')
+    return knex.schema.dropTable('report')
 }
